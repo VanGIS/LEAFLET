ced0183b176b1ebdc9349ae9446c5f9a85be3fc4
VanGIS <bretvan@email.arizona.edu>
2014-07-25 16:20:50 -0700

    initial commit

---
+++ single.js
@@ -0,0 +1,8 @@
+var single ={
+"type": "FeatureCollection",
+
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Leslie Stovall", "TITLE": "GIS Manager", "COMPANY": "Gila River Indian Community", "ADDRESS": "291 W Casa Blanca Rd", "ADDRESS_2": "Ki' Bldg 2", "CITY": "Sacaton", "STATE": "AZ", "ZIP": 85247, "CELL": null, "OFFICE": "520.562.6003", "EMAIL": "Leslie.Stovall@gric.nsn.us", "WEBSITE": "http:\/\/www.gilariver.org\/index.php\/departments-cols5-colw1190-colw2190-col3w190col4w190-col5w190-right0-tribal-departments\/84-department-of-land-a-water-resources", "LAT": 33.077628, "LONG": -111.742632 }, "geometry": { "type": "Point", "coordinates": [ -111.742632, 33.077628 ] } }
+]
+}
